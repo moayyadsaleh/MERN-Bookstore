5fd7d659d9a16cfe83f7d414a00ffdb0fcae3d03
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-09-10 19:39:07 -0600

    Connect to MongoDB, install dependencies, test and debug, create first post request, create schema, follow seperation of conserns for organized code base

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const uri =
+  "mongodb+srv://moayyedazzam:0S7oROl68buOOAfX@cluster0.dny87pr.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,54 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose"; // Import mongoose library
+import { Book } from "./models/bookModel.js";
+import { PORT, uri } from "./config.js";
 const app = express();
-app.listen(PORT, () => {
-  console.log(`App is listening to port ${PORT}`);
+///Middleware to parse incoming request body
+app.use(express.json());
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN Stack Tutorial");
 });
+
+//Route to save a new book
+app.post("/books", async (req, res) => {
+  try {
+    // Check if required fields are missing in the request body
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).json({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    // Create a new book using the 'Book' model, not 'Books'
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook); // Use 'Book' instead of 'Books'
+
+    // Send a successful response with the created book data
+    return res.status(201).json(book);
+  } catch (error) {
+    console.error(error.message); // Use console.error for error messages
+
+    // Send an error response with a status code of 500 and the error message
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Assuming 'app' is your Express.js application instance
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
