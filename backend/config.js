6ed6c4e834a2185ff54ae50858a8468be618eff0
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-09-14 11:37:13 -0600

    Add final version

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const uri =
-  "mongodb+srv://moayyedazzam:0S7oROl68buOOAfX@cluster0.dny87pr.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose"; // Import mongoose library
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoutes.js";
 import cors from "cors";
-import { PORT, uri } from "./config.js";
 const app = express();
 ///Middleware to parse incoming request body
 app.use(express.json());
@@ -11,18 +12,18 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(234).send("Welcome to MERN Stack Tutorial");
+  return res.status(234).send("App is up and running");
 });
 //Add middleware, for each books route, handle it with the booksRoute.
 app.use("/books", booksRoute);
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
 
     // Assuming 'app' is your Express.js application instance
-    app.listen(PORT, () => {
-      console.log(`App is listening to port ${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
